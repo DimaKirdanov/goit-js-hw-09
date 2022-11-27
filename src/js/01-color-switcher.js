b01927876e601f899f83f65c9b96754eba407669
DimaKirdanov <108092906+DimaKirdanov@users.noreply.github.com>
2022-11-27 20:33:27 +0200

    add color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+  
 
+    // colorValueText: document.querySelector(".color"),
+};
+
+let idStart = null;
+refs.btnStart.addEventListener('click', onClickBtnStart);
+refs.btnStop.addEventListener('click', onClickBtnStop);
+
+
+
+
+function onClickBtnStart() {
+  idStart = setInterval(() => {
+    refs.body.style.backgroundColor =
+        getRandomHexColor();
+    //   refs.colorValueText.textContent = getRandomHexColor();
+  }, 1000);
+
+  refs.btnStart.disabled = true;
+}
+
+function onClickBtnStop() {
+  refs.btnStart.disabled = false;
+  clearInterval(idStart);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
